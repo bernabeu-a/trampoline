d62711d2db583d8f990645536e6c27c6df369213
bernabeu-a <antoine.bernabeu@zoho.com>
2025-03-26 15:33:34 +0100

    [audiomoth] add cmsisdsp and cmsisnn, wip mnist

---
+++ examples/cortex/armv7em/efm32wg/audiomoth/mnist/main.cpp
@@ -0,0 +1,27 @@
+#include "tpl_os.h"
+#include <stdint.h>
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+
+#define APP_Task_mnist_START_SEC_CODE
+#include "tpl_memmap.h"
+
+FUNC(int, OS_APPL_CODE) main(void){
+	CHIP_Init();
+	// Enable Clock to GPIO
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	// Set GPIO Red Led to Output
+	GPIO->P[gpioPortC].MODEL |= 4 << (16);
+	StartOS(OSDEFAULTAPPMODE);
+	return 0;
+}
+
+TASK(mnist){
+	GPIO->P[gpioPortC].DOUT ^= 1<<4;
+
+
+	ChainTask(mnist);
+}
+#define APP_Task_mnist_STOP_SEC_CODE
+#include "tpl_memmap.h"
