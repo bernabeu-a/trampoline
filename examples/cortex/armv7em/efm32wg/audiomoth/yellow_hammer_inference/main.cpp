0cc431ec02910ceed2993a7e755eab31caf9b4c9
bernabeu-a <antoine.bernabeu@univ-nantes.fr>
2025-04-08 14:15:55 +0200

    example inference tflite audiomoth

--- examples/cortex/armv7em/efm32wg/audiomoth/yellow_hammer_inference/main.cpp
+++ examples/cortex/armv7em/efm32wg/audiomoth/yellow_hammer_inference/main.cpp
@@ -12,51 +12,14 @@
 #include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/micro_profiler.h"
-#include "tensorflow/lite/micro/recording_micro_interpreter.h"
 #include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
+/* Debugging */
+#include "tensorflow/lite/micro/recording_micro_interpreter.h"
+
 #include "input/input.h"
 #include "model/bands_int8_model_model_data.h"
-
-// void setup_model(){
-// 	model = tflite::GetModel(g_bands_int8_model_model_data);
-// 	// Check some stuff ? version etc ?
-
-// 	static tflite::MicroMutableOpResolver<23> micro_op_resolver;
-// 	micro_op_resolver.AddExpandDims();
-// 	micro_op_resolver.AddConv2D();
-// 	micro_op_resolver.AddMul();
-// 	micro_op_resolver.AddAdd();
-// 	micro_op_resolver.AddReshape();
-// 	micro_op_resolver.AddExpandDims();
-// 	micro_op_resolver.AddMaxPool2D();
-// 	micro_op_resolver.AddReshape();
-// 	micro_op_resolver.AddExpandDims();
-// 	micro_op_resolver.AddConv2D();
-// 	micro_op_resolver.AddMul();
-// 	micro_op_resolver.AddAdd();
-// 	micro_op_resolver.AddReshape();
-// 	micro_op_resolver.AddExpandDims();
-// 	micro_op_resolver.AddMaxPool2D();
-// 	micro_op_resolver.AddReshape();
-// 	micro_op_resolver.AddExpandDims();
-// 	micro_op_resolver.AddConv2D();
-// 	micro_op_resolver.AddReshape();
-// 	micro_op_resolver.AddMean();
-// 	micro_op_resolver.AddFullyConnected();
-// 	micro_op_resolver.AddFullyConnected();
-// 	micro_op_resolver.AddLogistic();
-
-// 	static tflite::MicroInterpreter static_interpreter(model, micro_op_resolver, tensor_arena, kTensorArenaSize);
-// 	interpreter = &static_interpreter;
-// 	TfLiteStatus allocate_status = interpreter->AllocateTensors();
-// 	input = interpreter->input(0);
-// 	return;
-// }
-
-#define APP_Task_inference_START_SEC_CODE
-#include "tpl_memmap.h"
 
 namespace {
 	using YellowHammerOpResolver = tflite::MicroMutableOpResolver<9>;
@@ -67,38 +30,67 @@ namespace {
 		op_resolver.AddMul();
 		op_resolver.AddAdd();
 		op_resolver.AddReshape();
-		// op_resolver.AddExpandDims();
 		op_resolver.AddMaxPool2D();
-		// op_resolver.AddReshape();
-		// op_resolver.AddExpandDims();
-		// op_resolver.AddConv2D();
-		// op_resolver.AddMul();
-		// op_resolver.AddAdd();
-		// op_resolver.AddReshape();
-		// op_resolver.AddExpandDims();
-		// op_resolver.AddMaxPool2D();
-		// op_resolver.AddReshape();
-		// op_resolver.AddExpandDims();
-		// op_resolver.AddConv2D();
-		// op_resolver.AddReshape();
 		op_resolver.AddMean();
 		op_resolver.AddFullyConnected();
-		// op_resolver.AddFullyConnected();
 		op_resolver.AddLogistic();
 		return kTfLiteOk;
 	}
 
 	tflite::MicroInterpreter *interpreter = nullptr;
-	// TfLiteTensor *input = nullptr;
-	// TfLiteTensor *output = nullptr;
+	TfLiteTensor *input = nullptr;
+	TfLiteTensor *output = nullptr;
 	const tflite::Model* model = nullptr;
-
+	TfLiteStatus myStatus;
 	// // Are of memory to use for input, output and intermediate array
 	// // Size is max output layer ?
 	// constexpr int kTensorArenaSize = 2720;
 	// alignas(16) static uint8_t tensor_arena[kTensorArenaSize];
 
 }	//namespace
+
+TfLiteStatus AllocateTensor(){
+	TF_LITE_ENSURE_STATUS(interpreter->AllocateTensors());
+}
+
+TfLiteStatus ProcessInference(){
+	TF_LITE_ENSURE_STATUS(interpreter->Invoke());
+}
+
+#define APP_Task_setup_inference_START_SEC_CODE
+#include "tpl_memmap.h"
+TASK(setup_inference){
+	/* Get Model */
+	model = ::tflite::GetModel(bands_int8_model_tflite);
+	if(model->version() != TFLITE_SCHEMA_VERSION){
+		while(1);
+	}
+	/* Ops */
+	YellowHammerOpResolver op_resolver;
+	RegisterOps(op_resolver);
+	/* ScratchPad Memory for Layers */
+	constexpr int kTensorArenaSize = 11500;
+	uint8_t tensor_arena[kTensorArenaSize];
+	static tflite::MicroInterpreter static_interpreter(model, op_resolver, tensor_arena, kTensorArenaSize);
+	interpreter = &static_interpreter;
+	myStatus = AllocateTensor();
+	if(myStatus != kTfLiteOk){
+		while(1);
+	}
+	/* Input */
+	input = interpreter->input(0);
+	TFLITE_CHECK_NE(input, nullptr);
+	/* Output */
+	output = interpreter->output(0);
+	TFLITE_CHECK_NE(output, nullptr);
+
+	ChainTask(inference);
+}
+#define APP_Task_setup_inference_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+#define APP_Task_inference_START_SEC_CODE
+#include "tpl_memmap.h"
 
 FUNC(int, OS_APPL_CODE) main(void){
 	CHIP_Init();
@@ -111,39 +103,9 @@ FUNC(int, OS_APPL_CODE) main(void){
 	return 0;
 }
 
-TfLiteStatus AllocateTensor(){
-	TF_LITE_ENSURE_STATUS(interpreter->AllocateTensors());
-}
-
-TfLiteStatus ProcessInference(){
-	TF_LITE_ENSURE_STATUS(interpreter->Invoke());
-}
-
 TASK(inference){	// 500ms
-	/* Get Model */
-	// const tflite::Model* model = ::tflite::GetModel(bands_int8_model_tflite);
-	model = ::tflite::GetModel(bands_int8_model_tflite);
-	if(model->version() != TFLITE_SCHEMA_VERSION){
-		while(1);
-	}
-	/* Ops */
-	YellowHammerOpResolver op_resolver;
-	RegisterOps(op_resolver);
-	/* ScratchPad Memory for Layers */
-	constexpr int kTensorArenaSize = 16000;
-	uint8_t tensor_arena[kTensorArenaSize];
+	GPIO->P[gpioPortA].DOUT |= (1<<7);
 
-	static tflite::MicroInterpreter static_interpreter(model, op_resolver, tensor_arena, kTensorArenaSize);
-	interpreter = &static_interpreter;
-	TfLiteStatus myStatus = AllocateTensor();
-	if(myStatus != kTfLiteOk){
-		while(1);
-	}
-	/* Input */
-	TfLiteTensor* input = interpreter->input(0);
-	TFLITE_CHECK_NE(input, nullptr);
-	/* Output */
-	TfLiteTensor* output = interpreter->output(0);
 	// Get data for input
 	uint16_t i;
 
@@ -154,7 +116,6 @@ TASK(inference){	// 500ms
 	float output_scale = 0.00390625;
 	int output_zero_point = -128;
 
-	GPIO->P[gpioPortA].DOUT |= 1<<7;
 
 	int8_t quantized_value[261] = {0};
 	for(i=0; i<261; i++){
@@ -170,13 +131,14 @@ TASK(inference){	// 500ms
 	if(myStatus != kTfLiteOk){
 		while(1);
 	}
-	// int8_t final_output;
-	// std::copy_n(tflite::GetTensorData<int8_t>(output), 1, &final_output);
+	// // int8_t final_output;
+	// // std::copy_n(tflite::GetTensorData<int8_t>(output), 1, &final_output);
 	int8_t final_output = output->data.int8[0];
 	float output_score = static_cast<float>(static_cast<int>(final_output) - output_zero_point) * output_scale;
 
 	if(output_score > 0.6f){
-		GPIO->P[gpioPortA].DOUT |= (1<<7);
+		// GPIO->P[gpioPortA].DOUT |= (1<<7);
+		GPIO->P[gpioPortA].DOUT &= ~(1<<7);
 	}
 	GPIO->P[gpioPortA].DOUT &= ~(1<<7);
 	ChainTask(inference);
